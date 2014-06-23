d0ebe2507bc2fa4605cf16d56178ed43bba4c90a
Mohsin <mohxin08@gmail.com>
2014-06-24 00:33:41 +0500

    - First commit

---
+++ application/views/layout.php
@@ -0,0 +1,259 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    <!-- Apple devices fullscreen -->
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <!-- Apple devices fullscreen -->
+    <meta names="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+    
+    <title>HOPE - KEMCAANA</title>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="<?php echo base_url(); ?>layout/css/bootstrap.min.css">
+    <!-- Bootstrap responsive -->
+    <link rel="stylesheet" href="<?php echo base_url(); ?>layout/css/bootstrap-responsive.min.css">
+    <!-- jQuery UI -->
+    <link rel="stylesheet" href="<?php echo base_url(); ?>layout/css/plugins/jquery-ui/smoothness/jquery-ui.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>layout/css/plugins/jquery-ui/smoothness/jquery.ui.theme.css">
+    	<!-- dataTables -->
+	<link rel="stylesheet" href="<?php echo base_url(); ?>layout/css/plugins/datatable/TableTools.css">
+    <!-- PageGuide -->
+    <link rel="stylesheet" href="<?php echo base_url(); ?>layout/css/plugins/pageguide/pageguide.css">
+    <!-- Fullcalendar -->
+    <link rel="stylesheet" href="<?php echo base_url(); ?>layout/css/plugins/fullcalendar/fullcalendar.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>layout/css/plugins/fullcalendar/fullcalendar.print.css" media="print">
+    <!-- chosen -->
+    <link rel="stylesheet" href="<?php echo base_url(); ?>layout/css/plugins/chosen/chosen.css">
+    <!-- select2 -->
+    <link rel="stylesheet" href="<?php echo base_url(); ?>layout/css/plugins/select2/select2.css">
+    <!-- icheck -->
+    <link rel="stylesheet" href="<?php echo base_url(); ?>layout/css/plugins/icheck/all.css">
+    <!-- Theme CSS -->
+    <link rel="stylesheet" href="<?php echo base_url(); ?>layout/css/style.css">
+    <!-- Color CSS -->
+    <link rel="stylesheet" href="<?php echo base_url(); ?>layout/css/themes.css">
+
+
+    <!-- jQuery -->
+    <script src="<?php echo base_url(); ?>layout/js/jquery.min.js"></script>
+    
+    
+    <!-- Nice Scroll -->
+    <script src="<?php echo base_url(); ?>layout/js/plugins/nicescroll/jquery.nicescroll.min.js"></script>
+    <!-- jQuery UI -->
+    <script src="<?php echo base_url(); ?>layout/js/plugins/jquery-ui/jquery.ui.core.min.js"></script>
+    <script src="<?php echo base_url(); ?>layout/js/plugins/jquery-ui/jquery.ui.widget.min.js"></script>
+    <script src="<?php echo base_url(); ?>layout/js/plugins/jquery-ui/jquery.ui.mouse.min.js"></script>
+    <script src="<?php echo base_url(); ?>layout/js/plugins/jquery-ui/jquery.ui.draggable.min.js"></script>
+    <script src="<?php echo base_url(); ?>layout/js/plugins/jquery-ui/jquery.ui.resizable.min.js"></script>
+    <script src="<?php echo base_url(); ?>layout/js/plugins/jquery-ui/jquery.ui.sortable.min.js"></script>
+    <script src="<?php echo base_url(); ?>layout/js/plugins/jquery-ui/jquery.ui.spinner.js"></script>
+    <!-- Touch enable for jquery UI -->
+    <script src="<?php echo base_url(); ?>layout/js/plugins/touch-punch/jquery.touch-punch.min.js"></script>
+    <!-- slimScroll -->
+    <script src="<?php echo base_url(); ?>layout/js/plugins/slimscroll/jquery.slimscroll.min.js"></script>
+    <!-- Bootstrap -->
+    <script src="<?php echo base_url(); ?>layout/js/bootstrap.min.js"></script>
+    <!-- vmap -->
+    <script src="<?php echo base_url(); ?>layout/js/plugins/vmap/jquery.vmap.min.js"></script>
+    <script src="<?php echo base_url(); ?>layout/js/plugins/vmap/jquery.vmap.world.js"></script>
+    <script src="<?php echo base_url(); ?>layout/js/plugins/vmap/jquery.vmap.sampledata.js"></script>
+    <!-- Bootbox -->
+    <script src="<?php echo base_url(); ?>layout/js/plugins/bootbox/jquery.bootbox.js"></script>
+    <!-- Flot -->
+    <script src="<?php echo base_url(); ?>layout/js/plugins/flot/jquery.flot.min.js"></script>
+    <script src="<?php echo base_url(); ?>layout/js/plugins/flot/jquery.flot.bar.order.min.js"></script>
+    <script src="<?php echo base_url(); ?>layout/js/plugins/flot/jquery.flot.pie.min.js"></script>
+    <script src="<?php echo base_url(); ?>layout/js/plugins/flot/jquery.flot.resize.min.js"></script>
+    <!-- imagesLoaded -->
+    <script src="<?php echo base_url(); ?>layout/js/plugins/imagesLoaded/jquery.imagesloaded.min.js"></script>
+    <!-- PageGuide -->
+    <script src="<?php echo base_url(); ?>layout/js/plugins/pageguide/jquery.pageguide.js"></script>
+    <!-- FullCalendar -->
+    <script src="<?php echo base_url(); ?>layout/js/plugins/fullcalendar/fullcalendar.min.js"></script>
+	<!-- dataTables -->
+	<script src="<?php echo base_url(); ?>layout/js/plugins/datatable/jquery.dataTables.min.js"></script>
+	<script src="<?php echo base_url(); ?>layout/js/plugins/datatable/TableTools.min.js"></script>
+	<script src="<?php echo base_url(); ?>layout/js/plugins/datatable/ColReorderWithResize.js"></script>
+	<script src="<?php echo base_url(); ?>layout/js/plugins/datatable/ColVis.min.js"></script>
+	<script src="<?php echo base_url(); ?>layout/js/plugins/datatable/jquery.dataTables.columnFilter.js"></script>
+	<script src="<?php echo base_url(); ?>layout/js/plugins/datatable/jquery.dataTables.grouping.js"></script>
+    <!-- Chosen -->
+    <script src="<?php echo base_url(); ?>layout/js/plugins/chosen/chosen.jquery.min.js"></script>
+    <!-- select2 -->
+    <script src="<?php echo base_url(); ?>layout/js/plugins/select2/select2.min.js"></script>
+    <!-- icheck -->
+    <script src="<?php echo base_url(); ?>layout/js/plugins/icheck/jquery.icheck.min.js"></script>
+
+    <!-- Theme framework -->
+    <script src="<?php echo base_url(); ?>layout/js/eakroko.min.js"></script>
+    <!-- Theme scripts -->
+    <script src="<?php echo base_url(); ?>layout/js/application.min.js"></script>
+    <!-- Just for demonstration -->
+    <script src="<?php echo base_url(); ?>layout/js/demonstration.min.js"></script>
+    
+    <!--[if lte IE 9]>
+        <script src="js/plugins/placeholder/jquery.placeholder.min.js"></script>
+        <script>
+            $(document).ready(function() {
+                $('input, textarea').placeholder();
+            });
+        </script>
+    <![endif]-->
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="<?php echo base_url(); ?>layout/img/favicon.ico" />
+    <!-- Apple devices Homescreen icon -->
+    <link rel="apple-touch-icon-precomposed" href="<?php echo base_url(); ?>layout/img/apple-touch-icon-precomposed.png" />
+
+</head>
+
+<body>
+    
+    <div id="navigation">
+        <div class="container-fluid">
+            <a href="#" id="brand">FLAT</a>
+            <a href="#" class="toggle-nav" rel="tooltip" data-placement="bottom" title="Toggle navigation"><i class="icon-reorder"></i></a>
+            <ul class='main-nav'>
+                <li class='active'>
+                    <a href="/">
+                        <span>Dashboard</span>
+                    </a>
+                </li>               
+            </ul>
+            <div class="user">
+                <ul class="icon-nav">
+                    <li class='dropdown'>
+                        <a href="#" class='dropdown-toggle' data-toggle="dropdown"><i class="icon-envelope"></i><span class="label label-lightred">4</span></a>
+                        <ul class="dropdown-menu pull-right message-ul">
+                            <li>
+                                <a href="#">
+                                    <img src="<?php echo base_url(); ?>layout/img/demo/user-1.jpg" alt="">
+                                    <div class="details">
+                                        <div class="name">Jane Doe</div>
+                                        <div class="message">
+                                            Lorem ipsum Commodo quis nisi ...
+                                        </div>
+                                    </div>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <img src="<?php echo base_url(); ?>layout/img/demo/user-2.jpg" alt="">
+                                    <div class="details">
+                                        <div class="name">John Doedoe</div>
+                                        <div class="message">
+                                            Ut ad laboris est anim ut ...
+                                        </div>
+                                    </div>
+                                    <div class="count">
+                                        <i class="icon-comment"></i>
+                                        <span>3</span>
+                                    </div>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <img src="<?php echo base_url(); ?>layout/img/demo/user-3.jpg" alt="">
+                                    <div class="details">
+                                        <div class="name">Bob Doe</div>
+                                        <div class="message">
+                                            Excepteur Duis magna dolor!
+                                        </div>
+                                    </div>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="components-messages.html" class='more-messages'>Go to Message center <i class="icon-arrow-right"></i></a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+                <div class="dropdown">
+                    <a href="#" class='dropdown-toggle' data-toggle="dropdown">John Doe <img src="<?php echo base_url(); ?>layout/img/demo/user-avatar.jpg" alt=""></a>
+                    <ul class="dropdown-menu pull-right">
+                        <li>
+                            <a href="more-userprofile.html">Edit profile</a>
+                        </li>
+                        <li>
+                            <a href="#">Account settings</a>
+                        </li>
+                        <li>
+                            <a href="more-login.html">Sign out</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid" id="content">
+        <div id="left">
+            <form action="search-results.html" method="GET" class='search-form'>
+                <div class="search-pane">
+                    <input type="text" name="search" placeholder="Search here...">
+                    <button type="submit"><i class="icon-search"></i></button>
+                </div>
+            </form>
+            <div class="subnav">
+                <div class="subnav-title">
+                    <a href="#" class='toggle-subnav'><i class="icon-angle-down"></i><span>Content</span></a>
+                </div>
+                <ul class="subnav-menu">
+                    <li>
+                        <a href="/index.php/offers/index">Offers</a>
+                    </li>
+                    <li>
+                        <a href="/index.php/doctors/index">Doctors</a>
+                    </li>
+
+                    <li>
+                        <a href="/index.php/students/index">Students</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="subnav">
+                <div class="subnav-title">
+                    <a href="#" class='toggle-subnav'><i class="icon-angle-down"></i><span>Utilities</span></a>
+                </div>
+                <ul class="subnav-menu">
+                    <li>
+                        <a href="doctors-spec.html">Doctor Specialities</a>
+                    </li>
+                    <li>
+                        <a href="student-spec.html">Student Specialities</a>
+                    </li>                   
+                </ul>
+            </div>          
+        </div>
+        <div id="main">
+            <div class="container-fluid">
+                <div class="page-header">
+                    <div class="pull-left">
+                        <h1>Dashboard</h1>
+                    </div>
+                </div>
+                <div class="breadcrumbs">
+                    <ul>
+                        <li>
+                            <a href="more-login.html">Home</a>
+                            <i class="icon-angle-right"></i>
+                        </li>
+                        <li>
+                            <a href="index.html">Dashboard</a>
+                        </li>
+                    </ul>
+                    <div class="close-bread">
+                        <a href="#"><i class="icon-remove"></i></a>
+                    </div>
+                </div>  
+                {content}
+            </div>
+        </div>
+        
+    </div>
+        
+    </body>
+
+</html>
